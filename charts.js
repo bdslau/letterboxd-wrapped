1586ad9763613285fb8f58da9b4f74229ab56f57
bdslau <pegalebareiro@gmail.com>
2025-01-07 15:12:07 -0300

    Fixed path genres rated json file

--- charts.js
+++ charts.js
@@ -134,7 +134,7 @@ function drawCharts() {
 
 
     // PLOT: Highest rated GENRES
-    fetch('/assets/docs/genresRated_data.json')
+    fetch('/letterboxd-wrapped/assets/docs/genresRated_data.json')
         .then(response => {
             if (!response.ok) throw new Error('Failed to load genresRated_data.json');
             return response.json();
